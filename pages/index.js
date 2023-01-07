daeec2a355542056581c35d6d477de6fd6a158f9
Marcel Weber <mrweber@gmx.ch>
2023-01-07 15:52:45 +0100

    Changed structure a bit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from "next/image"
 import s from '../styles/Home.module.css'
 import Gruppenfoto from "../public/Gruppenfoto.jpg"
+import Logo from "../public/logo.gif"
 import News from "../components/News"
 
 export default function Home() {
@@ -13,11 +14,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 >Matchschützenvereinigung Schaffhausen</h1>
+      <h1 ><div className={s.logo}><Image src={Logo} fill /></div>Matchschützenvereinigung Schaffhausen</h1>
         <main>
           
         <section className={s.section}>
-          <h2>Home</h2>            
+          <h2>Home</h2> 
+          <div className={s.image}>
+          <Image src={Gruppenfoto} fill/>   
+          </div>        
             <div className={s.container}>
               <button className={s.button}>Kantonalcup</button>
               <button className={s.button}>Mitglied werden</button>
