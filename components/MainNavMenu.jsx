15b1cc574879bd5790cb1067d0f63609d59f2077
Marcel Weber <mrweber@gmx.ch>
2023-01-07 15:50:26 +0100

    Fleshed out Navbar component

--- components/MainNavMenu.jsx
+++ components/MainNavMenu.jsx
@@ -1,16 +1,51 @@
+import { useState, useEffect } from "react"
 import Link from "next/link"
-import styles from '../styles/Navbar.module.css'
+import s from "../styles/Navbar.module.css"
 
-export default function Nav(){
+export default function Navbar(){
+
+    const [overTrigger, setOverTrigger] = useState(false) // checks if cursor is over trigger link
+    const [visible, setVisible] = useState(false) // checks if submenu is visible
+
+    function handleSubMenuTrigger(e){
+        if(e.type == "mouseenter"){
+            setOverTrigger(true)
+        } else if(e.type == "mouseleave"){
+            setOverTrigger(false)
+        }
+    }
+
+    useEffect(()=>{
+        if(overTrigger){
+            setVisible(true)
+        }
+        if(!overTrigger){
+            setVisible(false)
+        }
+    },[overTrigger])
+
+
     return(
-        <nav className={styles.navbar}>
-            <ul className={styles.navbarList}>
-                <li className={styles.navbarItem}>
-                    <Link href="/" className={styles.navbarItemLink}>
-                        Home
-                    </Link>
-                </li>
-            </ul>
+        <nav className ={s.nav}>
+            <div className={s.linkContainer}>
+                <Link className={s.homeLink} href="/"></Link>
+                <Link className={s.link} href="/anlaesse">Gewehr</Link>
+                <Link className={s.link} href="/resultate">Pistole</Link>
+                <Link className={s.link} href="/resultate">LG & Lupi</Link>
+                <Link className={s.link} href="/resultate">Kantonalcup</Link>
+                <div className={s.surLinkContainer} onMouseEnter={(e)=>handleSubMenuTrigger(e)} onMouseLeave={(e)=>handleSubMenuTrigger(e)}>
+                    <div className={s.link} >Die MSVS</div>
+                    {visible?
+                    <div className={s.subLinkContainer} >
+                        <Link className={s.sublink} href="/schiessen/schwabenkrieg-erinnerungsschiessen">Jubiläum</Link>
+                        <Link className={s.sublink} href="/schiessen/obligatorisches">Vorstand</Link>
+                        <Link className={s.sublink} href="/schiessen/feldschiessen">Bilder</Link>
+                        <Link className={s.sublink} href="/schiessen/hilfsmittel">Protokolle</Link>
+                    </div>
+                    : null}
+                </div>
+                <Link className={s.link} href="/links">Links</Link>
+            </div>
         </nav>
     )
 }
