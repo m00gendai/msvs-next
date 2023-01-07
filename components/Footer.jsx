ce525784faf7810588b057a7b03bd57bbefde5ad
Marcel Weber <mrweber@gmx.ch>
2023-01-07 16:03:10 +0100

    News footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+import s from "../styles/Footer.module.css"
+import { useMediaQuery } from '@react-hook/media-query'
+
+export default function Footer(){
+
+    const isMobile = useMediaQuery('only screen and (orientation: portrait)')
+    const date = new Date()
+    const currentYear = date.getFullYear()
+
+    return(
+        <footer className={s.footer}>
+            {
+                isMobile
+                ?
+                <>
+                    <p>
+                        ©{currentYear.toString() == "2023" ? "2023" : `2023 - ${currentYear}`} Pistolenclub Hallau
+                    </p>
+                    <div className={s.linkContainer}>
+                        <Link className={s.link} href="/impressum">Impressum</Link>
+                        <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
+                    </div>
+                </>
+                :
+                <>
+                    <Link className={s.link} href="/impressum">Impressum</Link>
+                    <p>
+                        ©{currentYear.toString() == "2023" ? "2023" : `2023 - ${currentYear}`} Matschschützenvereinigung Schaffhausen
+                    </p>
+                    <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
+                </>
+            }
+            
+        </footer>
+    )
+}
