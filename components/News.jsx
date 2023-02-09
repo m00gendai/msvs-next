5116f404040468a04c12cae5f8d57c0627eb888f
Marcel Weber <mrweber@gmx.ch>
2023-02-09 13:30:33 +0100

    Made it so latest files are displayed in news

--- components/News.jsx
+++ components/News.jsx
@@ -1,11 +1,16 @@
 import s from "../styles/News.module.css"
+import getFile from "../functions/getFile"
 
-export default function News(){
+export default function News({items}){
+
     return(
         <div className={s.container}>
         <h2>News</h2>
         <div className={s.inner}>
-            
+            {items.map(item =>{
+                const name = item.name.replaceAll("_", " ").replace(".pdf", "")
+                return <p key={item.id} onClick={()=>getFile(item.id)}>{name}</p>
+            })}
         </div>
         </div>
     )
