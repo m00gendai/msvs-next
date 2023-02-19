6e876b7f39a11bafa1d65e275df42584fc2bc1c9
Marcel Weber <mrweber@gmx.ch>
2023-02-19 19:35:57 +0100

    Added content for Vorstand page

--- pages/vorstand.js
+++ pages/vorstand.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
-import s from "../styles/Page.module.css"
+import s from "../styles/Vorstand.module.css"
+import { boardmembers } from "../lib/vorstand"
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 export default function Vorstand(){
+  console.log(boardmembers)
     return(
         <>
       <Head>
@@ -13,6 +18,27 @@ export default function Vorstand(){
         <main>
         <section className={s.section}>
           <h2>Vorstand</h2> 
+          <div className={s.container}>
+            {
+              boardmembers.map(member =>{
+                const physicalAddress = member.where.split("<br>")
+                const street = physicalAddress[0]
+                const place = physicalAddress[1]
+                return(
+                  <div className={s.item}>
+                    <h3>{member.what}</h3>
+                    <h4>{member.who}</h4>
+                    <div className={s.doxx}>
+                      {member.where.length > 0 ? <div className={s.infoAds}><p>{street}</p><p>{place}</p></div> : null}
+                      {member.mobile.length > 0 ? <div className={s.info}><PhoneAndroidIcon /><p>{member.mobile}</p></div> : null}
+                      {member.landline.length > 0 ? <div className={s.info}><PhoneIcon /><p>{member.landline}</p></div> : null}
+                      {member.mail.length > 0 ? <div className={s.info}><AlternateEmailIcon /><p>{member.mail}</p></div> : null}
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
         </section>
         </main>
 
