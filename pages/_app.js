6b5a51e7449c6c06505da70943de9e51ead39d35
Marcel Weber <mrweber@gmx.ch>
2023-02-20 14:53:38 +0100

    added nav switch

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import Navbar from "../components/Navbar"
+import Navbar_Mobile from "../components/navbar_mobile"
 import Footer from "../components/Footer"
+import { useMediaQuery } from '@react-hook/media-query'
 
 export default function App({ Component, pageProps }) {
+
+  const isMobile = useMediaQuery('only screen and (max-aspect-ratio: 13/9)')
+
   return(
     <>
-    <Navbar />
+    {isMobile ?
+        <Navbar_Mobile />
+      :
+        <Navbar />
+    }
     <Component {...pageProps} />
     <Footer />
     </>
