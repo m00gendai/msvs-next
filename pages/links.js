ca6ff6487ac33809855970a08894525dda1a8eeb
Marcel Weber <mrweber@gmx.ch>
2023-02-21 13:24:52 +0100

    Populated Link page

--- pages/links.js
+++ pages/links.js
@@ -1,18 +1,40 @@
-import Head from 'next/head'
-import s from "../styles/Page.module.css"
+import Link from "next/link"
+import { useRouter } from 'next/router'
+import Header from "../components/header"
+import s from "../styles/Links.module.css"
+import { links } from "../lib/links"
 
 export default function Links(){
+
+  const router = useRouter()
+    const headUrl = `https://msvs.ch${router.pathname}`
+
     return(
         <>
-      <Head>
-        <title>Matchschützenvereinigung Schaffhausen</title>
-        <meta name="description" content="Matchschützenvereinigung Schaffhausen" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Header title={"MSVS - Links"} content={"MSVS - Links"} url={headUrl} />
         <main>
         <section className={s.section}>
           <h2>Links</h2> 
+          {
+            links.map(link =>{
+              return (
+                <div className={s.container}>
+                  <h3>{link.type}</h3>
+                  <div className={s.inner}>
+                    {link.content.map(content =>{
+                      return (
+                        <Link className={s.item} href={content.url} target={"_blank"}>
+                          <div className={s.itemInner} style={{backgroundImage: `url("${content.img}")`}}>
+                            <p>{content.name}</p>
+                          </div>
+                        </Link>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            })
+          }
         </section>
         </main>
 
