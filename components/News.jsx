413226da02dfc0a908adaf4816f7b0cb58034e80
Marcel Weber <mrweber@gmx.ch>
2023-02-26 01:36:42 +0100

    Added loading spinner component

--- components/News.jsx
+++ components/News.jsx
@@ -1,8 +1,7 @@
 import s from "../styles/News.module.css"
 import getFile from "../functions/getFile"
 
-export default function News({items}){
-
+export default function News({setShow, items}){
     return(
         <div className={s.container}>
         <h2>News</h2>
@@ -16,7 +15,7 @@ export default function News({items}){
                     .replace(".docx", "")
                     .replace(".doc", "")
                 return (
-                    <div className={s.item} key={item.id} onClick={()=>getFile(item.id)}>
+                    <div className={s.item} key={item.id} onClick={()=>getFile(item.id, setShow)}>
                         <p>{name}</p>
                     </div>
                 )
