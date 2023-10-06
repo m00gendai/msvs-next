eb41d36693677bf593177061ef2276f26b05c1f9
Marcel Weber <mrweber@gmx.ch>
2023-10-07 01:40:44 +0200

    Massive refactor Reworked fetch logic to only consider data of current year to minimize data return size Reworked fetch logic makes file rendering substantially easier and puts less load on client Removed yearPicker functionality, it doesnt scale with pages router

--- pages/pistole.js
+++ pages/pistole.js
@@ -1,141 +1,75 @@
-import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from 'next/router'
-import { useState } from "react"
 import Header from "../components/header"
 import s from "../styles/Page.module.css"
 import getFile from "../functions/getFile"
 import extensionTrimmer from "../functions/extensionTrimmer"
-import YearPicker from "../components/YearPicker"
 
 export default function Pistole(
     {
         setShow,
-        sourceDirectoryList, 
-        images
+        resultCurrentYearDirectories,
+        getResultFiles,
+        getInvitationFiles
     }
     ){
 
     const date = new Date()
-    const [currentYear, setCurrentYear] = useState(date.getFullYear())
-
+    const currentYear = date.getFullYear()
     const router = useRouter()
     const headUrl = `https://msvs.ch${router.pathname}`
-
-    const results = sourceDirectoryList.data.sort((a,b) =>{
-        const x = a.added_at
-        const y = b.added_at
-        return x < y ? 1 : x > y ? -1 : 0
-    })
-
-    const currentYearDirs = results.filter(result =>{ // This filters for all directories of the current year
-        return result.name == currentYear.toString()
-    })
-
-    const currentYearDirIds = currentYearDirs.map(item=>{ // this extracts the parent_id of the current year directories
-        return item.parent_id
-    })
-
-    const einladungen = results.filter(result =>{
-        return result.type == "dir" && isNaN(parseInt(result.name)) && currentYearDirIds.includes(result.id) && result.name == "Einladungen"
-    })
-
-    const resultate = results.filter(result =>{
-       return result.type == "dir" && isNaN(parseInt(result.name)) && currentYearDirIds.includes(result.id) && result.name != "Einladungen"
-    })
 
     return(
         <>
         <Header title={"MSVS - Pistole"} content={"MSVS Pistole"} url={headUrl} />
         <main>
             <section className={s.section}>
-                <h2>{`Pistole`}<YearPicker currentYear={currentYear} setCurrentYear={setCurrentYear}/></h2>
-                <h3>{`Einladungen ${currentYear}`}</h3>
-                {
-                    einladungen.length == 0 ?
-                    <p className ="noEntry">Noch keine Einladungen dieses Jahr.</p> :
-                    results.map(result =>{
-                        if(result.type == "dir" && isNaN(parseInt(result.name)) && currentYearDirIds.includes(result.id) && result.name == "Einladungen"){ 
-                        /* If directory AND the directory name is not a number AND there is a subdirectory with the name of the current 
-                            year present (see parent_id filtering above) */
-                            return (
-                                <div className={s.results} key={`einladungFragment_${result.id}`}>
-                                    <div className={s.container} key={`einladungContainer_${result.id}`}>
-                                    {
-                                        results.map(result2 =>{
-                                            if(result2.type == "dir" && result2.name == currentYear.toString() && result2.parent_id == result.id){
-                                                return results.map(result3 =>{
-                                                    if(result3.type == "file" && result3.parent_id == result2.id){
-                                                        
-                                                        return (
-                                                            <div key={`einladung_${result3.id}`} className={s.item} onClick={()=>getFile(result3.id, setShow)}>
-                                                                <div className={s.text}>
-                                                                    {extensionTrimmer(result3.name)}
-                                                                </div>
-                                                            </div>
-                                                        )
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }
-                                    </div>
-                                </div>
-                            )
+                <h2>{`Pistole`}</h2>
+                <h3>Einladungen</h3>
+                <div className={s.results}>
+                    <div className={s.container}>
+                        {
+                            getInvitationFiles.map(invitations =>{
+                                return invitations.data.map(invitation=>{
+                                    return(
+                                        <div key={`invitation_${invitation.id}`} onClick={()=> getFile(invitation.id, setShow)} className={s.item}>
+                                            <div className={s.text}>
+                                                {extensionTrimmer(invitation.name)}
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            })
                         }
-                    })
-                }
+                    </div>
+                </div>
                 <h3>{`Resultate ${currentYear}`}</h3>
-                {
-                    resultate.length == 0 ?
-                    <p className="noEntry">Noch keine Resultate dieses Jahr.</p> :
-                    results.map(result =>{
-                        if(result.type == "dir" && isNaN(parseInt(result.name)) && currentYearDirIds.includes(result.id) && result.name != "Einladungen"){ 
-                        /* If directory AND the directory name is not a number AND there is a subdirectory with the name of the current 
-                            year present (see parent_id filtering above) */
-                            return (
-                                <div className={s.results} key={`imageFragment_${result.id}`}>
-                                    <h4 key={`imageTitle_${result.name}`}>{result.name}</h4> {/* only displayed if there is something for the current year */}
-                                    <div key={`imageContainer_${result.name}`} className={s.imageContainer}>
-                                    {
-                                        results.map(result2 =>{
-                                            if(result2.type == "dir" && result2.name == currentYear.toString() && result2.parent_id == result.id){
-                                                return images.map(img =>{
-                                                    if(img.parent == result2.id){
-                                                        return <img className={s.image} src={`data:$;base64, ${img.string}`} key={`imageItem_${img.id}`}/>
-                                                    }
-                                                })
+                 {
+                    resultCurrentYearDirectories.data.map(dir =>{
+                        return (
+                            <div className={s.results} key={`Resultate_${dir.id}`}>
+                                <h4>{dir.path.split("/")[dir.path.split("/").length-2]}</h4>
+                                <div className={s.container}>
+                                {
+                                    getResultFiles.map(results =>{
+                                        return results.data.map(result =>{
+                                            if(result.parent_id == dir.id){
+                                                return(
+                                                    <div key={`Resultat_${result.id}`} onClick={()=> getFile(result.id, setShow)} className={s.item}>
+                                                        <div className={s.text}>
+                                                            {extensionTrimmer(result.name)}
+                                                        </div>
+                                                    </div>
+                                                )
                                             }
                                         })
-                                    }
-                                    </div>
-                                    <div key={`linkContainer_${result.name}`} className={s.container}>
-                                    {
-                                        results.map(result2 =>{
-                                            if(result2.type == "dir" && result2.name == currentYear.toString() && result2.parent_id == result.id){
-                                                return results.map(result3 =>{
-                                                    if(result3.type == "file" && result3.parent_id == result2.id){
-                                                       
-                                                        if(!result3.mime_type.startsWith("image")){
-                                                            return(
-                                                                <div key={`result_${result3.id}`} className={s.item} onClick={()=>getFile(result3.id, setShow)}>
-                                                                    <div className={s.text}>
-                                                                        {extensionTrimmer(result3.name)}
-                                                                    </div>
-                                                                </div>
-                                                            )
-                                                        }
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }
-                                    </div>
+                                    })
+                                }
                                 </div>
-                            )
-                        }
+                            </div>
+                        )
                     })
-                }
+                 }
                 <Link className="archiv" href={`https://kdrive.infomaniak.com/app/share/608492/7d5ad3ba-c137-4d02-b7ca-7bd9664817c7`} target={`_blank`} ><h3>Archiv</h3></Link>
             </section>
         </main>
@@ -145,47 +79,57 @@ export default function Pistole(
 
 export async function getStaticProps() {
 
-    // Gets all folders and files in the /Pistole directory recursively, sorted by last modified
-    const getSourceDirectoryList = await fetch("https://api.infomaniak.com/2/drive/608492/files/search?directory_id=15714&depth=unlimited&per_page=1000", {
+    const date = new Date()
+    const currentYear = date.getFullYear()
+
+    const getResultsCurrentYearDirectories = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/search?with=path&type=dir&query=${currentYear}&directory_id=${process.env.KDRIVE_PIST_RES}`, {
         method: "GET",
         headers: {
             Authorization: `Bearer ${process.env.KDRIVE}`,
             "Content-Type" : "application/json"
         },
-
     })
-    const sourceDirectoryList = await getSourceDirectoryList.json()
 
-    // Gets all images in the /Pistole directory recursively, sorted by last modified
-    const getSourceImageList = await fetch("https://api.infomaniak.com/2/drive/608492/files/search?directory_id=15714&depth=unlimited&per_page=1000&types[]=image", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${process.env.KDRIVE}`,
-            "Content-Type" : "application/json"
-        },
+    const resultCurrentYearDirectories = await getResultsCurrentYearDirectories.json()
 
-    })
-    const sourceImageList = await getSourceImageList.json()
-
-    // Gets preview Image object for each image file
-    const images = await Promise.all(sourceImageList.data.map(async image =>{
-        let getImg = await fetch(`https://api.infomaniak.com/2/drive/608492/files/${image.id}/preview`, {
+    const getResultFiles = await Promise.all(resultCurrentYearDirectories.data.map(async dir =>{
+        const getDirectoryFiles = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/${dir.id}/files`,{
             method: "GET",
             headers: {
                 Authorization: `Bearer ${process.env.KDRIVE}`,
                 "Content-Type" : "application/json"
             },
         })
-        // Magic by https://stackoverflow.com/questions/72036447/createobjecturl-error-argument-must-be-an-instance-of-blob-received-an-instan
-        getImg = await getImg.arrayBuffer()
-        const img = Buffer.from(getImg).toString("base64")
-        return {id: image.id, parent: image.parent_id, string: img}
+        return await getDirectoryFiles.json()
+    }))
+
+    const getInvitationCurrentYearDirectories = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/search?with=path&type=dir&query=${currentYear}&directory_id=${process.env.KDRIVE_PIST_INV}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${process.env.KDRIVE}`,
+            "Content-Type" : "application/json"
+        },
+    })
+
+    const invitationCurrentYearDirectories = await getInvitationCurrentYearDirectories.json()
+
+    const getInvitationFiles = await Promise.all(invitationCurrentYearDirectories.data.map(async dir =>{
+        const getDirectoryFiles = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/${dir.id}/files`,{
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.KDRIVE}`,
+                "Content-Type" : "application/json"
+            },
+        })
+        return await getDirectoryFiles.json()
     }))
 
     return { 
         props: {
-            sourceDirectoryList, images
+            resultCurrentYearDirectories,
+            getResultFiles,
+            getInvitationFiles
         } , 
-            revalidate: 2
+        revalidate: 10
     }
 }
