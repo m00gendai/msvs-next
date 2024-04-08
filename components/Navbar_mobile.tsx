eed51c45614b5444b918eb56eb59a94f4ed61893
m00gendai <mrweber@gmx.ch>
2024-04-09 01:09:36 +0200

    Refactored to Typescript

--- components/Navbar_mobile.tsx
+++ components/Navbar_mobile.tsx
@@ -1,17 +1,18 @@
+"use client"
+
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import s from "../styles/navbar_mobile.module.css"
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 import LunchDiningIcon from '@mui/icons-material/LunchDining';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import React from "react";
 
 export default function Navbar_Mobile(){
 
-    const [overTrigger, setOverTrigger] = useState(false) // checks if cursor is over trigger link
-    const [visible, setVisible] = useState(false) // checks if submenu is visible
+    const [overTrigger, setOverTrigger] = useState<boolean>(false) // checks if cursor is over trigger link
+    const [visible, setVisible] = useState<boolean>(false) // checks if submenu is visible
 
-    function handleSubMenuTrigger(e){
+    function handleSubMenuTrigger(e:React.MouseEvent){
         if(e.type == "mouseenter"){
             setOverTrigger(true)
         } else if(e.type == "mouseleave"){
@@ -38,7 +39,7 @@ export default function Navbar_Mobile(){
             :
                 <div className={s.homeLink}><Link className={s.homeLinkLogo} href="/"></Link>MSVS</div>
             }   
-            <div className={s.menu} onClick={(e)=>handleSubMenuTrigger(e)}>
+            <div className={s.menu} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)}>
                 {
                 visible ?
                     <LocalDiningIcon sx={{
@@ -55,19 +56,19 @@ export default function Navbar_Mobile(){
             {
             visible ?
                 <div className={s.linkContainer}>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/">Home</Link>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/gewehr">Gewehr</Link>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/pistole">Pistole</Link>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/luft">Luft</Link>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/kantonalcup">Kantonalcup</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/">Home</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/gewehr">Gewehr</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/pistole">Pistole</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/luft">Luft</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/kantonalcup">Kantonalcup</Link>
                     <div className={s.link} >die MSVS</div>
                     <div className={s.subLinkContainer} >
-                        <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/vorstand">Vorstand</Link>
-                        <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/jubilaeum">Jubiläum</Link>
-                        <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/bilder">Bilder</Link>
-                        <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/dokumente">Dokumente</Link>
+                        <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/vorstand">Vorstand</Link>
+                        <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/jubilaeum">Jubiläum</Link>
+                        <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/bilder">Bilder</Link>
+                        <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/dokumente">Dokumente</Link>
                     </div>
-                    <Link className={s.link} onClick={(e)=>handleSubMenuTrigger(e)} href="/links">Links</Link>
+                    <Link className={s.link} onClick={(e:React.MouseEvent)=>handleSubMenuTrigger(e)} href="/links">Links</Link>
                 </div>
             : 
                 null
