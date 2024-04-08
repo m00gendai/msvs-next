0a87ffde8662c805eae22b506229817313734d8d
m00gendai <mrweber@gmx.ch>
2024-04-09 01:51:55 +0200

    Refactored News component Data fetching is now directly within the component

---
+++ components/News.tsx
@@ -0,0 +1,35 @@
+import s from "../styles/News.module.css"
+import { FileResponse } from "../interfaces"
+import News_Button from "./News_Button"
+
+async function getFiles(){
+    const getSourceDirectoryList:Response = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/search?directory_id=${process.env.KDRIVE_NEWS}&depth=unlimited&order_by=added_at&order=desc&types[]=pdf&types[]=spreadsheet&types[]=text&per_page=12`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${process.env.KDRIVE}`,
+            "Content-Type" : "application/json"
+        },
+
+    })
+    const sourceDirectoryList:FileResponse = await getSourceDirectoryList.json()
+    return sourceDirectoryList
+}
+
+export default async function News(){
+
+    const items:FileResponse =  await getFiles()
+
+    return(
+        <div className={s.container}>
+        <h2>News</h2>
+        <div className={s.inner}>
+        {items.data.map(item =>{
+                return(
+                    /*@ts-expect-error*/
+                   <News_Button item={item} />
+                )
+            })}
+        </div>
+        </div>
+    )
+}
