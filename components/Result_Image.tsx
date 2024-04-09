0713ea7ef86740391a92b9f13a7dc60a73f177b9
m00gendai <mrweber@gmx.ch>
2024-04-09 04:11:43 +0200

    Created Result Image component

---
+++ components/Result_Image.tsx
@@ -0,0 +1,40 @@
+import { GetFileResponse, File } from "../interfaces"
+import s from "../styles/Page.module.css"
+import Image from "next/image"
+
+async function getImage(id:number){
+    const getUrl = await fetch(`https://api.infomaniak.com/2/drive/${process.env.KDRIVE_ROOT}/files/${id}/temporary_url`,{
+        method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.KDRIVE}`,
+                "Content-Type" : "application/json",
+            },
+        })
+        const url:GetFileResponse = await getUrl.json()
+        console.log(id)
+        if(url.result === "error"){
+            return `${process.env.ARCHIVE}`
+        }
+        return url.data.temporary_url 
+    }
+
+interface Props{
+    file: File
+}
+
+export default async function Result_Image({file}:Props){
+    const path:string = await getImage(file.id)
+    
+    return(
+        
+            <div className={s.imageItem} key={`image_${file.id}`}>
+                <Image 
+                    src={path}
+                    alt={"Bild"}
+                    fill={true}
+                    style={{objectFit: "contain"}}
+                />
+            </div>
+        
+    )
+}
