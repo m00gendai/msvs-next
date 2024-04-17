a72fde16bed773bf48ebd96a2317155e86fd9cc2
m00gendai <mrweber@gmx.ch>
2024-04-17 18:27:35 +0200

    Added interface for CMS image

--- interfaces.ts
+++ interfaces.ts
@@ -68,3 +68,35 @@ export interface Board{
   _cby: string
   _id: string
 }
+
+export interface LinkData{
+  name: string
+  url: string
+  image: Medium
+  category: string
+  _modified: number
+  _mby: string
+  _created: number
+  _state: number
+  _cby: string
+  _id: string
+}
+
+export interface Medium {
+  path: string
+  title: string
+  mime: string
+  type: string
+  description: string
+  tags: string[]
+  size: number
+  colors: string[]
+  width: number
+  height: number
+  _hash: string
+  _created: number
+  _modified: number
+  _cby: string
+  folder: string
+  _id: string
+}
