d6ed04217b537cde50db76ea205dcfa1ff67dace
m00gendai <mrweber@gmx.ch>
2024-04-19 12:48:25 +0200

    Reworked winter masters rendering Added metadata generation

--- app/luft/page.tsx
+++ app/luft/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link"
 import Invitaitons from "../../components/Invitations"
 import Results from "../../components/Results"
+import Results_WM from "../../components/Results_WM"
+import { getPageMetadata } from "../../functions/getPageMetadata"
+
+export async function generateMetadata(){
+    return getPageMetadata("Luft")
+}
 
 export default async function Page(){
 
@@ -12,7 +18,7 @@ export default async function Page(){
             <section className="section">
                 <h2>{`Luft`}</h2>
                 <Invitaitons drive={process.env.KDRIVE_AIR_INV} currentYear={currentYear} />
-                {/* TODO: Wintermeisterschaft */}
+                <Results_WM drive={process.env.KDRIVE_AIR_WM} currentYear={currentYear} />
                 <Results drive={process.env.KDRIVE_AIR_RES} currentYear={currentYear} />
                 <Link className="archiv" href={`https://kdrive.infomaniak.com/app/share/${process.env.KDRIVE_ROOT}/225d4d16-dff3-47ab-ae41-773504b219d5`} target={`_blank`} ><h3>Archiv</h3></Link>
             </section>
