553e44a1a73f646b5c74318299855f1952b84f23
m00gendai <mrweber@gmx.ch>
2024-04-23 19:46:53 +0200

    Created Suspense Loader Fallback Components

---
+++ components/Loader_Result.tsx
@@ -0,0 +1,18 @@
+import s from "../styles/Page.module.css"
+
+export default async function Result_Container(){
+
+    return(
+        <>
+        <h3>Resultate</h3>
+        <div className={s.results}>
+            <h4>...</h4>
+            <div className={s.container}>
+                <div className={s.fileContainer}>
+                ...
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
