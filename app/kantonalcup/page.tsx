cc7ec8f3460e05dc5e7ce2e5c366654dab7e6195
m00gendai <mrweber@gmx.ch>
2024-04-23 19:47:23 +0200

    Added Suspense Loaders

--- app/kantonalcup/page.tsx
+++ app/kantonalcup/page.tsx
@@ -2,6 +2,9 @@ import Link from "next/link"
 import Cup_Invitations from "../../components/Cup_Invitations"
 import Cup_Results from "../../components/Cup_Results"
 import { getPageMetadata } from "../../functions/getPageMetadata"
+import Loader_Invite from "../../components/Loader_Invite"
+import Loader_Result from "../../components/Loader_Result"
+import { Suspense } from "react"
 
 export async function generateMetadata(){
     return getPageMetadata("Kantonalcup")
@@ -16,8 +19,12 @@ export default async function Page(){
         <main>
             <section className="section">
                 <h2>{`Kantonalcup`}</h2>
+                <Suspense fallback={<Loader_Invite />}>
                 <Cup_Invitations currentYear={currentYear} drive={process.env.KDRIVE_CUP} />
+                </Suspense>
+                <Suspense fallback={<Loader_Result />}>
                 <Cup_Results currentYear={currentYear} drive={process.env.KDRIVE_CUP} />
+                </Suspense>
                 <Link className="archiv" href={`https://kdrive.infomaniak.com/app/share/${process.env.KDRIVE_ROOT}/225d4d16-dff3-47ab-ae41-773504b219d5`} target={`_blank`} ><h3>Archiv</h3></Link>
             </section>
         </main>
