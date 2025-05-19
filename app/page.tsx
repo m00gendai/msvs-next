6082433956da19c0fd7094902d32c8678208301d
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-05-19 20:42:12 +0000

    Added SPecial Hero Image functionality

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import MSVSP from "../public/MSVSP.jpg"
 import MSVSG from "../public/MSVSG.jpg"
 import Logo from "../public/logo.gif"
 import News from "../components/News"
-import { FileResponse, GetFileResponse } from "../interfaces"
+import { FileResponse, GetFileResponse, Medium, SpecialHeroImage } from "../interfaces"
 import { getPageMetadata } from "../functions/getPageMetadata"
 import { Suspense } from "react"
 import Loader_News from "../components/Loader_News"
@@ -39,6 +39,25 @@ async function getFilePath(id:number){
         return url.data.temporary_url 
     }
 
+async function getSpecialHeroImage(date:number){
+    const getUrl = await fetch(`https://cms.msvs.ch/api/content/items/specialHero`)
+    const fileResponse = await getUrl.json()
+
+    console.log(fileResponse)
+    if(fileResponse.length != 0){
+        const file:SpecialHeroImage = fileResponse[0]
+        const from:number = new Date(file.validityFrom).getTime()
+        const to:number = new Date(file.validityUntil).getTime()
+        if(date >= from && date <= to){
+            return file
+        } else {
+            return null
+        }
+    } else {
+        return null
+    }
+}
+
 export async function generateMetadata(){
     return getPageMetadata("Home")
 }
@@ -47,6 +66,8 @@ export default async function Home() {
 
     const thatFile:FileResponse = await getJoinFile()
     const path:string = await getFilePath(thatFile.data[0].id)
+    const specialHeroImage:SpecialHeroImage | null = await getSpecialHeroImage(new Date().getTime())
+console.log(specialHeroImage)
   
     return (
     <>
@@ -54,8 +75,8 @@ export default async function Home() {
         <main>
         <section className={s.section}>
           <h2>Home</h2> 
-          <Link className={s.image} href="/jubilaeum">
-          <Image src={Math.floor(Math.random() * 100)%2 === 0 ? MSVSG : MSVSP} fill={true} style={{objectFit: "cover"}} alt="Introbilder" priority/>   
+          <Link className={s.image} style={{backgroundColor: `${specialHeroImage ? "black" : "yellow"}`}} href={specialHeroImage ? `/${specialHeroImage.link}` : "/"}>
+          <Image src={specialHeroImage ? `https://cms.msvs.ch/storage/uploads${specialHeroImage.heroImage.path}` : Math.floor(Math.random() * 100)%2 === 0 ? MSVSG : MSVSP} fill={true} style={{objectFit: specialHeroImage ? "contain" : "cover"}} alt="Introbilder" priority/>   
           </Link>        
             <div className={s.container}>
               <Link href="/kantonalcup" className={s.button}>Kantonalcup</Link>
